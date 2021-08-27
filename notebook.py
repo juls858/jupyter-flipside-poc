ed1c9af4aa026a92cc51cd25eecbe139b2d1db79
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-08-27 11:19:45 -0700

    - switched to conda environment - removed requests

--- notebook.py
+++ notebook.py
@@ -1,21 +1,18 @@
 # %%
 import pandas as pd
-import requests
 
-# pd.options.plotting.backend = "plotly"
 # %% [markdown]
 """
 # Get Data From Flipside API
 """
 # %%
 api = "https://api.flipsidecrypto.com/api/v2/queries/882af4ed-5981-4815-b74c-f06a3ed2c9e6/data/latest"
-response = requests.get(api)
 # %% [markdown]
 """
 Create Dataframe
 """
 # %%
-df = pd.DataFrame(response.json())
+df = pd.read_json(api)
 df["BLOCK_HOUR"] = pd.to_datetime(df["BLOCK_HOUR"])
 # %% [markdown]
 """
@@ -31,4 +28,3 @@ Check latest record
 df["BLOCK_HOUR"].max()
 # %%
 _ = df.set_index("BLOCK_HOUR").plot.line(title="Amount Transferred By Hour")
-# %%
