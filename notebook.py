65d18fd133f72f0e9bdff2d2035686cb84f13285
Julius Remigio <14811322+juls858@users.noreply.github.com>
2021-09-17 10:01:46 -0700

    - added auto refresh script

--- notebook.py
+++ notebook.py
@@ -1,19 +1,41 @@
 # %%
+%%html
+<script>
+    // AUTORUN ALL CELLS ON NOTEBOOK-LOAD!
+    require(
+        ['base/js/namespace', 'jquery'], 
+        function(jupyter, $) {
+            $(jupyter.events).on("kernel_ready.Kernel", function () {
+                console.log("Auto-running all cells-below...");
+                jupyter.actions.call('jupyter-notebook:run-all-cells-below');
+                jupyter.notebook.scroll_to_top();
+                jupyter.actions.call('jupyter-notebook:save-notebook');                
+                
+            });
+        });
+        
+        $( document ).ready(function(){
+        code_shown=false;
+        $('div.input').hide()});
+    
+    
+</script>
+# %%
 import pandas as pd
 
+pd.options.plotting.backend = "plotly"
 # %% [markdown]
 """
 # Get Data From Flipside API
 """
 # %%
-api = "https://api.flipsidecrypto.com/api/v2/queries/882af4ed-5981-4815-b74c-f06a3ed2c9e6/data/latest"
+api = "https://api.flipsidecrypto.com/api/v2/queries/00880fc5-94d5-46ce-8e23-734a79ae1237/data/latest"
 # %% [markdown]
 """
 Create Dataframe
 """
 # %%
 df = pd.read_json(api)
-df["BLOCK_HOUR"] = pd.to_datetime(df["BLOCK_HOUR"])
 # %% [markdown]
 """
 Preview data
@@ -22,9 +44,17 @@ Preview data
 df.head()
 # %% [markdown]
 """
-Check latest record
+Create plot
+"""
+# %% [markdown]
+"""
+Verify data we have today's data
 """
 # %%
-df["BLOCK_HOUR"].max()
+df["DATE"].max()
 # %%
-_ = df.set_index("BLOCK_HOUR").plot.line(title="Amount Transferred By Hour")
+df.plot.line(
+    x="DATE", y="N_TRANSACTION", title="Matic – Daily Transactions (Last 30 Days)"
+)
+
+# %%
